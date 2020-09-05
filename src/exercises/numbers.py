d7caf3fb6dc9fefdd50d2619146cbbae3fa9ad18
Vitor Della Torre dos Santos <47284767+vdella@users.noreply.github.com>
2020-09-05 16:13:56 -0300

    Initial commit

---
+++ src/exercises/numbers.py
@@ -0,0 +1,74 @@
+# coding=utf-8
+
+# Exercício 1:
+# Jorge quer escrever um programa que retorna um valor booleano
+# caso um dado número, enviado pelo usuário, seja um número primo
+# ou não.
+def is_a_prime_number(number):
+    """
+    TODO
+     Complete a função de modo que se "number" for primo seja
+     retornado True.
+     Caso contrário, retorne False.
+    """
+    return number
+
+
+# Exercício 2:
+# A famosa Fabiana foi a uma festa com o intuito de ficar com Jorge, da situação problema anterior.
+# Jorge é uma pessoa difícil e pediu que Fabiana resolvesse o seguinte exercício para decidir.
+def fib(number):
+    """
+    TODO
+     Você deve arranjar uma forma de retornar o fibonacci de um valor chamado "number".
+     Há muitas, muitas formas desse exercício ser realizado, sendo a mais otimizada pouco trivial e, portanto,
+     não é o que queremos no momento. QUERO QUE SEJA RESOLVIDO DE FORMA ITERATIVA
+     OU RECURSIVA DO JEITO QUE ACHAR MELHOR.
+    """
+    return number
+
+
+# Exercício 3:
+# 30 anos depois de Fabiana e Jorge se casarem, seu filho Matheus pede ajuda num problema:
+# ele relembra dos exercícios que seus pais fizeram e gostaria de saber se poderiam ser combinados.
+# Fabiana e Jorge ficam receosos e hesitantes, mas aceitam o desafio pelo bem da educação de seu filho.
+def prime_number_fib(number):
+    """
+    TODO
+     Será dado um número qualquer à função.
+     1. SE ELE FOR PRIMO, calcule o fibonacci dele e retorne o valor calculado.
+     2. SE ELE NÃO FOR PRIMO, retorne -1.
+    """
+    return number
+
+
+# Exercício 4:
+# Estamos no futuro. Matheus conseguiu descobrir a fórmula para a produção de água em massa e agora temos água
+# desidratada, é um milagre. Mas mais um desafio aparece. Um forasteiro aparece a cavalo nas portas de seu laboratório
+# e diz: "Tens a manha?" Mas, claro, que sim. E Matheus lhe escuta dizer o seguinte:
+def area(shape, *value_according_to_shape):
+    """
+    TODO
+     O forasteiro criou uma convenção e disse a Matheus:
+     * Se "shape" for 0, vou querer o valor da área de um CÍRCULO, e seu RAIO será representado pelo primeiro valor
+     "value_according_to_shape"
+     * Se "shape" for 1, vou querer o valor da área de um TRIÂNGULO, e sua ALTURA será representado pelo primeiro valor
+     "value_according_to_shape", ENQUANTO SUA ALTURA SERÁ O SEGUNDO VALOR.
+     * Se "shape" for 2, vou querer o valor da área de um QUADRADO, e UM DE SEUS LADOS será representado pelo primeiro
+      valor "value_according_to_shape".
+     * Caso não seja passado um dos valores requeridos, quero um erro, não um "print(...)".
+    """
+    return shape, value_according_to_shape
+
+
+# Exercício 5:
+# Numa navegação fenícia, durante o período do ápice de sua civilizaçào, Ulisseu navegava em seu navio com preocupação.
+# Havia um exercício da escolazinha que não tinha conseguido fazer ainda. Ao terminar a venda dos caramujos que coletara
+# e a produção de pigmentação que venderia para roupas, sentou-se em frente ao seu computador e leu:
+def sum_as_much_as_possible(*args):
+    """
+    TODO
+     Recebendo SOMENTE UM PARÂMETRO, retorne a soma múltiplos números ao final da função. Vale notar que há um asterisco
+     antes do nome do parâmetro. Por quê? Que função que você conhece que é parecida com essa?
+    """
+    return args
